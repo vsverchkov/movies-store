ed00cba595bf8df15f961c752775f41a9395f0f5
ValeriiSverchkov <valerysverchkov@gmail.com>
2022-11-30 07:10:51 +0200

    Added implementation of FindAll movies

--- pkg/services/movies.go
+++ pkg/services/movies.go
@@ -1,7 +1,9 @@
 package services
 
 import (
-	"math/rand"
+    "fmt"
+    "github.com/neo4j-graphacademy/neoflix/pkg/ioutils"
+    "math/rand"
 
 	"github.com/neo4j-graphacademy/neoflix/pkg/fixtures"
 
@@ -42,17 +44,47 @@ func NewMovieService(loader *fixtures.FixtureLoader, driver neo4j.Driver) MovieS
 // signify whether the user has added the movie to their "My Favorites" list.
 // tag::all[]
 func (ms *neo4jMovieService) FindAll(userId string, page *paging.Paging) (_ []Movie, err error) {
-	// TODO: Open an Session
-	// TODO: Execute a query in a new Read Transaction
-	// TODO: Get a list of Movies from the Result
-	// TODO: Close the session
+    session := ms.driver.NewSession(neo4j.SessionConfig{})
+    defer func() {
+        err = ioutils.DeferredClose(session, err)
+    }()
 
-	popularMovies, err := ms.loader.ReadArray("fixtures/popular.json")
-	if err != nil {
-		return nil, err
-	}
+    results, err := session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+        result, err := tx.Run(fmt.Sprintf(`
+            MATCH (m:Movie)
+            WHERE m.`+"`%[1]s`"+` IS NOT NULL
+            RETURN m { .* } AS movie
+            ORDER BY m.`+"`%[1]s`"+` %s
+            SKIP $skip
+            LIMIT $limit
+            `,
+            page.Sort(),
+            page.Order()),
+            map[string]interface{}{
+                "skip":  page.Skip(),
+                "limit": page.Limit(),
+            })
+        if err != nil {
+            return nil, err
+        }
 
-	return fixtures.Slice(popularMovies, page.Skip(), page.Limit()), err
+        records, err := result.Collect()
+        if err != nil {
+            return nil, err
+        }
+
+        var results []map[string]interface{}
+        for _, record := range records {
+            movie, _ := record.Get("movie")
+            results = append(results, movie.(map[string]interface{}))
+        }
+        return results, nil
+    })
+    if err != nil {
+        return nil, err
+    }
+
+    return results.([]Movie), nil
 }
 
 // end::all[]
